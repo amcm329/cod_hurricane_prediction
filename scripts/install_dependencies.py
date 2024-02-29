10c9a1543f243fd3dad2b6c41953a60bd66c0803
amcm329 <amcm329@hotmail.com>
2024-02-29 13:28:30 -0600

    Update install_dependencies.py

--- scripts/install_dependencies.py
+++ scripts/install_dependencies.py
@@ -1,1 +1,20 @@
 
+# Install the requirements
+
+!pip install --upgrade pip
+!pip install --no-cache-dir --log --progress-bar off scripts/pip-req.log -r requirements.txt
+
+import os
+import json
+import requests
+
+# Install cmlapi package
+try:
+    import cmlapi
+except ModuleNotFoundError:
+    import os
+    cluster = os.getenv("CDSW_API_URL")[:-1]+"2"
+    !pip3 install {cluster}/python.tar.gz
+    import cmlapi 
+  
+from cmlapi.rest import ApiException
