ac14f7eba4c3316e56f97c27cec520bd199690c8
amcm329 <amcm329@hotmail.com>
2024-03-03 12:31:34 -0600

    Update 01_install_dependencies.py

--- scripts/01_install_dependencies.py
+++ scripts/01_install_dependencies.py
@@ -3,3 +3,9 @@
 
 !pip install --upgrade pip
 !pip install --no-cache-dir --progress-bar off -r requirements.txt
+
+try:
+     import cml.data_v1 as cmldata
+     SPARK_CONNECTION_NAME = os.getenv("SPARK_CONNECTION_NAME")
+     conn = cmldata.get_connection(SPARK_CONNECTION_NAME)
+    except: 
