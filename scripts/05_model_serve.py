986e31b8d6079f62dec47c4d3c650498a7985d74
amcm329 <amcm329@hotmail.com>
2024-03-03 16:10:54 -0600

    Update 05_model_serve.py

--- scripts/05_model_serve.py
+++ scripts/05_model_serve.py
@@ -39,16 +39,16 @@ model = None
 #path with the prebuilt elements. 
 
 if os.getenv("USE_PREBUILT_MODEL") == "yes": 
-    ct = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/prebuilt-models/ct.joblib")
+    model = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/prebuilt-models/ct.joblib")
 
 else: 
-    ct = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/models/ct.joblib")
+    model = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/models/ct.joblib")
 
 if os.getenv("USE_PREBUILT_MODEL") == "yes": 
-    pipe = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/prebuilt-models/pipe.joblib")
+    pipeline = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/prebuilt-models/pipe.joblib")
 
 else: 
-    pipe = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/models/pipe.joblib")
+    pipeline = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/models/pipe.joblib")
 
 
 #This doesn't start with slash
@@ -57,8 +57,40 @@ else:
 
 #args = {"feature": "-50,50,100"}
 
+#--------------------------------------------------------------------------------------------------------
+
+        latitude = flask.request.form['latitude']
+        longitude = flask.request.form['longitude']
+        pressure = flask.request.form['pressure']
+        avg_pressure = flask.request.form['avgpressure']
+        air_temperature = flask.request.form['airtemperature']
+        dew_point = flask.request.form['dewpoint']
+        humidity = flask.request.form['humidity']
+        wind_direction = flask.request.form['winddirection']
+        avg_windspeed = flask.request.form['avgwindspeed']
+        wind_speed_ratio = flask.request.form['windspeedchange']
+
+        input_variables = [[latitude, longitude, pressure, air_temperature, dew_point,
+                            humidity, 0, 0, wind_direction, avg_pressure, avg_windspeed, wind_speed_ratio,
+                            ]]
+
+        transformed_variables = pipeline.transform(input_variables)
+
+        prediction = model.predict(transformed_variables)[0]
+
+        return flask.render_template('index.html',
+                                     original_input = {'Latitude':latitude,'Longitude':longitude,'Pressure':pressure, 
+                                                      'Average Pressure':avg_pressure,'Air Temperature':air_temperature,
+                                                      'Dew Point':dew_point,'Relative Humidity':humidity,'Wind Direction':wind_direction,
+                                                      'Average Wind Speed':avg_windspeed,'Wind Speed Change Ratio':wind_speed_ratio},
+                                     result = prediction,
+                                     latitude_x = latitude,
+                                     longitude_y = longitude)
+
+#---------------------------------------------------------------------------------------------------------
+
 @cdsw.model_metrics
-def predict_cancelled(args):
+def predict_wind_speed(args):
     inputs = args["feature"].split(",")
 
     latitude = float(inputs[0])
