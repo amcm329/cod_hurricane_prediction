b012554e6c976b2df13746e6da7fb108247ec4e6
amcm329 <amcm329@hotmail.com>
2024-03-03 16:19:36 -0600

    Update 05_model_serve.py

--- scripts/05_model_serve.py
+++ scripts/05_model_serve.py
@@ -1,19 +1,6 @@
 """
 In this code the generation of an endpoint for the model prediction takes place.
 """
-
-import numpy as np
-import pandas as pd
-import cdsw
-#import cml.metrics_v1 as metrics
-#import cml.models_v1 as models
-import json
-from joblib import dump, load
-import pickle
-
-# args = {"feature" : "US,DCA,BOS,1,16"}
-#Homedir is /home/cdsw
-
 import os
 
 #Detecting the operating system full path:
@@ -31,24 +18,35 @@ if full_path is None:
    os.environ["USE_PREBUILT_MODEL"] = "yes"
 
 
-ct = None
-pipe = None 
+import numpy as np
+import pandas as pd
+import cdsw
+#import cml.metrics_v1 as metrics
+#import cml.models_v1 as models
+import json
+from joblib import dump, load
+import pickle
+
+# args = {"feature" : "US,DCA,BOS,1,16"}
+#Homedir is /home/cdsw
+
 model = None 
+pipeline = None 
 
 #Reading both model and pipeline objects. Unless we are told otherwise, we take the 
 #path with the prebuilt elements. 
 
 if os.getenv("USE_PREBUILT_MODEL") == "yes": 
-    model = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/prebuilt-models/ct.joblib")
+    model = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/prebuilt-models/ensemble_model3.pkl")
 
 else: 
-    model = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/models/ct.joblib")
+    model = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/models/ensemble_model3.pkl")
 
 if os.getenv("USE_PREBUILT_MODEL") == "yes": 
-    pipeline = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/prebuilt-models/pipe.joblib")
+    pipeline = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/prebuilt-models/pipeline.pkl")
 
 else: 
-    pipeline = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/models/pipe.joblib")
+    pipeline = load(os.getenv("OPERATING_SYSTEM_PATH") + "src/models/pipeline.pkl")
 
 
 #This doesn't start with slash
@@ -57,38 +55,6 @@ else:
 
 #args = {"feature": "-50,50,100"}
 
-#--------------------------------------------------------------------------------------------------------
-
-        latitude = flask.request.form['latitude']
-        longitude = flask.request.form['longitude']
-        pressure = flask.request.form['pressure']
-        avg_pressure = flask.request.form['avgpressure']
-        air_temperature = flask.request.form['airtemperature']
-        dew_point = flask.request.form['dewpoint']
-        humidity = flask.request.form['humidity']
-        wind_direction = flask.request.form['winddirection']
-        avg_windspeed = flask.request.form['avgwindspeed']
-        wind_speed_ratio = flask.request.form['windspeedchange']
-
-        input_variables = [[latitude, longitude, pressure, air_temperature, dew_point,
-                            humidity, 0, 0, wind_direction, avg_pressure, avg_windspeed, wind_speed_ratio,
-                            ]]
-
-        transformed_variables = pipeline.transform(input_variables)
-
-        prediction = model.predict(transformed_variables)[0]
-
-        return flask.render_template('index.html',
-                                     original_input = {'Latitude':latitude,'Longitude':longitude,'Pressure':pressure, 
-                                                      'Average Pressure':avg_pressure,'Air Temperature':air_temperature,
-                                                      'Dew Point':dew_point,'Relative Humidity':humidity,'Wind Direction':wind_direction,
-                                                      'Average Wind Speed':avg_windspeed,'Wind Speed Change Ratio':wind_speed_ratio},
-                                     result = prediction,
-                                     latitude_x = latitude,
-                                     longitude_y = longitude)
-
-#---------------------------------------------------------------------------------------------------------
-
 @cdsw.model_metrics
 def predict_wind_speed(args):
     inputs = args["feature"].split(",")
@@ -96,16 +62,47 @@ def predict_wind_speed(args):
     latitude = float(inputs[0])
     longitude = float(inputs[1])
     pressure = float(inputs[2])
-    
-    input_variables = [[latitude, longitude, pressure]]
-    
-    # Falta scaler ojo!
-    prediction = model.predict(input_variables)[0]
+    avg_pressure = float(inputs[3])
+    air_temperature = float(inputs[4])
+    dew_point = float(inputs[5])
+    humidity = float(inputs[6])
+    wind_direction = float(inputs[7])
+    avg_windspeed = float(inputs[8])
+    wind_speed_ratio = float(inputs[9])
 
+    input_variables = [[latitude, longitude, pressure, air_temperature, dew_point, humidity, 0, 0, wind_direction, avg_pressure, avg_windspeed, wind_speed_ratio]]
+
+    transformed_variables = pipeline.transform(input_variables)
+
+    prediction = model.predict(transformed_variables)[0]
+     
     cdsw.track_metric("input_data", args)
-    cdsw.track_metric("prediction", int(prediction))
+    cdsw.track_metric("prediction", float(prediction))
+
+    original_input = ,
+                                     result = prediction,
+                                     latitude_x = latitude,
+                                     longitude_y = longitude 
     
-    response = {"prediction": int(prediction)}
+    response = {
+                 "original_input": {
+                                    'Latitude': latitude,
+                                    'Longitude': longitude,
+                                    'Pressure': pressure, 
+                                    'Average Pressure': avg_pressure,
+                                    'Air Temperature': air_temperature,
+                                    'Dew Point': dew_point,
+                                    'Relative Humidity': humidity,
+                                    'Wind Direction': wind_direction,
+                                    'Average Wind Speed':avg_windspeed,
+                                    'Wind Speed Change Ratio':wind_speed_ratio
+                                   }
+         
+         
+                 "result": float(prediction),
+                 "latitude_x": latitude,
+                 "longitude_y": longitude
+               }
 
     return response
 
