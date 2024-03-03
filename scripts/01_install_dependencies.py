1790925f83f7f785d4f7f2905200d5342789f531
amcm329 <amcm329@hotmail.com>
2024-03-03 12:34:44 -0600

    Update 01_install_dependencies.py

--- scripts/01_install_dependencies.py
+++ scripts/01_install_dependencies.py
@@ -1,11 +1,15 @@
 
-# Install the requirements located in the file requirements.txt
 
+# Install the requirements located in the file requirements.txt
 !pip install --upgrade pip
 !pip install --no-cache-dir --progress-bar off -r requirements.txt
 
+#Section for environment variables: 
+import os
+
 try:
-     import cml.data_v1 as cmldata
-     SPARK_CONNECTION_NAME = os.getenv("SPARK_CONNECTION_NAME")
-     conn = cmldata.get_connection(SPARK_CONNECTION_NAME)
-    except: 
+     #Element exists.
+     full_path = os.getenv("OPERATING_SYSTEM_PATH")
+except: 
+     #Element doesn't exist.
+     os.environ["OPERATING_SYSTEM_PATH"] = "/home/cdsw/"
