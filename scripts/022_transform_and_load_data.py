eb5aa7a72fb23628e7738c3b545f76df0b9bde64
amcm329 <amcm329@hotmail.com>
2024-03-03 18:59:12 -0600

    Update 022_transform_and_load_data.py

--- scripts/022_transform_and_load_data.py
+++ scripts/022_transform_and_load_data.py
@@ -8,6 +8,15 @@
   5.- Appends Meteostat information to the dataframe.
 """
 
+#Section for environment variables: 
+import os
+
+full_path = os.getenv("OPERATING_SYSTEM_PATH")
+     
+if full_path is None: 
+   #Element doesn't exist.
+   os.environ["OPERATING_SYSTEM_PATH"] = "/home/cdsw/"
+  
 import wget
 import meteostat
 import pandas as pd
