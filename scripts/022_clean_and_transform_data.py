b688a6c876bb53c7227e1f25eb7e27275793c83f
amcm329 <amcm329@hotmail.com>
2024-03-03 18:13:01 -0600

    Update 022_clean_and_transform_data.py

--- scripts/022_clean_and_transform_data.py
+++ scripts/022_clean_and_transform_data.py
@@ -1,10 +1,22 @@
 """
 """
 
-!scrapy runspider scraper.py
+def clean_and_transform_data():
+  
+    !scrapy runspider scraper.py
 
-handler = open()
+    handler = open()
 
-filename = wget.download(complete_url, out=splitted_element)
+    filename = wget.download(complete_url, out=splitted_element)
 
+
+if __name__ == "__main__":
+
+    #If the option is whether explicit or it doesn't exist, we use the prebuilt model.
+    if os.environ["CLEAN_AND_TRANSFORM_DATA"] == "yes" or os.environ["CLEAN_AND_TRANSFORM_DATA"] is None:
+        print("Using default dataset")
+         
+    else:
+        clean_and_transform_data()
+      
 
